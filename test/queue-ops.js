740b5eb20b5e2194c7945c21d3acff32d4b61c1a
xuthief <xuthief@gmail.com>
2014-06-23 12:07:44 +0800

    test case done

--- test/queue-ops.js
+++ test/queue-ops.js
@@ -6,7 +6,7 @@ describe('#lenqueue/ldequeue/lremove/lget', function() {
 
     it('should fail to lremove/ldequeue/lget to not existed key', function(done) {
         var cb = H.client;
-        var key = H.genKey("queue_key1");
+        var key = H.genKey("queue_key1_");
 
         cb.remove(key, function(){
             cb.lget(key, function(err, result) {
@@ -19,19 +19,19 @@ describe('#lenqueue/ldequeue/lremove/lget', function() {
         });
     });
 
-    it('should succ to lenqueue new key, get/dequeue to existed key', function(done) {
+    it('should succ to lenqueue new key, lget/dequeue existed key', function(done) {
         var cb = H.client;
-        var key = H.genKey("queue_key2");
+        var key = H.genKey("queue_key2_");
         var value = bignum("FFFEFDFCF0F1F2F3", 16);
         cb.remove(key, function(){
             cb.lenqueue(key, value, function(err, result) {
                 assert.equal(err, undefined, "should not return err: " + err);
                 cb.lget(key, function(err, result) {
                     assert.equal(err, undefined, "should not return err: " + err);
-                    assert.equal(result.value, [value], "should get same:", result.value, "!=", [value]);
+                    assert.deepEqual(result.value, [value], "should get same:", result.value, "!=", [value]);
                     cb.ldequeue(key, function(err, result){
                         assert.equal(err, undefined, "should not return err: " + err);
-                        assert.equal(result.value, value, "should dequeue same:", result.value, "!=", value);
+                        assert.deepEqual(result.value, value, "should dequeue same:", result.value, "!=", value);
                         cb.lget(key, function(err, result) {
                             assert.equal(err.code, H.errors.keyNotFound);
                             done();
@@ -42,9 +42,9 @@ describe('#lenqueue/ldequeue/lremove/lget', function() {
         });
     });
 
-    it('should succ to lenqueue old key, get/dequeue to existed key', function(done) {
+    it('should succ to lenqueue old key, lget/dequeue existed key', function(done) {
         var cb = H.client;
-        var key = H.genKey("queue_key3");
+        var key = H.genKey("queue_key3_");
         var value = bignum("FFFEFDFCF0F1F2F3", 16);
         cb.lenqueue(key, value, function(err, result) {
             assert.equal(err, undefined, "should not return err: " + err);
@@ -52,13 +52,13 @@ describe('#lenqueue/ldequeue/lremove/lget', function() {
                 assert.equal(err, undefined, "should not return err: " + err);
                 cb.lget(key, function(err, result) {
                     assert.equal(err, undefined, "should not return err: " + err);
-                    assert.equal(result.value, [value, value], "should get same:", result.value, "!=", [value, value]);
+                    assert.deepEqual(result.value, [value, value], "should get same:", result.value, "!=", [value, value]);
                     cb.ldequeue(key, function(err, result){
                         assert.equal(err, undefined, "should not return err: " + err);
-                        assert.equal(result.value, value, "should dequeue same:", result.value, "!=", value);
+                        assert.deepEqual(result.value, value, "should dequeue same:", result.value, "!=", value);
                         cb.ldequeue(key, function(err, result){
                             assert.equal(err, undefined, "should not return err: " + err);
-                            assert.equal(result.value, value, "should dequeue same:", result.value, "!=", value);
+                            assert.deepEqual(result.value, value, "should dequeue same:", result.value, "!=", value);
                             cb.lget(key, function(err, result) {
                                 assert.equal(err.code, H.errors.keyNotFound);
                                 done();
@@ -70,22 +70,22 @@ describe('#lenqueue/ldequeue/lremove/lget', function() {
         });
     });
 
-    it('should succ to lenqueue new key, get/remove to existed key', function(done) {
+    it('should succ to lenqueue new key, lget/remove existed key', function(done) {
         var cb = H.client;
-        var key = H.genKey("queue_key4");
+        var key = H.genKey("queue_key4_");
         var value = bignum("FFFEFDFCF0F1F2F3", 16);
         cb.remove(key, function(){
             cb.lenqueue(key, value, function(err, result) {
                 assert.equal(err, undefined, "should not return err: " + err);
                 cb.lget(key, function(err, result) {
                     assert.equal(err, undefined, "should not return err: " + err);
-                    assert.equal(result.value, [value], "should get same:", result.value, "!=", [value]);
+                    assert.deepEqual(result.value, [value], "should get same:", result.value, "!=", [value]);
                     var evalue = value.add(1);
                     cb.lremove(key, evalue, function(err, result){
                         assert.equal(err, undefined, "should not return err: " + err);
                         cb.lget(key, function(err, result) {
                             assert.equal(err, undefined, "should not return err: " + err);
-                            assert.equal(result.value, [value], "should get same:", result.value, "!=", [value]);
+                            assert.deepEqual(result.value, [value], "should get same:", result.value, "!=", [value]);
                             cb.lremove(key, value, function(err, result){
                                 assert.equal(err, undefined, "should not return err: " + err);
                                 cb.lget(key, function(err, result) {
@@ -100,9 +100,9 @@ describe('#lenqueue/ldequeue/lremove/lget', function() {
         });
     });
 
-    it('should succ to lenqueue old key, get/dequeue to existed key', function(done) {
+    it('should succ to lenqueue old key, lget/remove existed key', function(done) {
         var cb = H.client;
-        var key = H.genKey("queue_key5");
+        var key = H.genKey("queue_key5_");
         var value1 = bignum("FFFEFDFCF0F1F2F1", 16);
         var value2 = value1.add(1);
         var value3 = value2.add(1);
@@ -120,14 +120,14 @@ describe('#lenqueue/ldequeue/lremove/lget', function() {
                         assert.equal(err, undefined, "should not return err: " + err);
                         cb.lget(key, function(err, result) {
                             assert.equal(err, undefined, "should not return err: " + err);
-                            assert.equal(result.value, [value1, value2, value1], "should get same:", 
+                            assert.deepEqual(result.value, [value1, value2, value1], "should get same:", 
                                 result.value, "!=", [value1, value2, value1]);
                             // remove value1 -> [value2]
                             cb.lremove(key, value1, function(err, result){
                                 assert.equal(err, undefined, "should not return err: " + err);
                                 cb.lget(key, function(err, result) {
                                     assert.equal(err, undefined, "should not return err: " + err);
-                                    assert.equal(result.value, [value2], "should get same:", 
+                                    assert.deepEqual(result.value, [value2], "should get same:", 
                                         result.value, "!=", [value2]);
                                     // remove value1 -> []
                                     cb.lremove(key, value2, function(err, result){
